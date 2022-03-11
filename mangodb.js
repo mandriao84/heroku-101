80a399ef5a0b0bb779b59923fb2134be7420c523
mandriao84 <gregory@inqom.com>
2022-03-11 15:25:34 +0100

    update

--- mangodb.js
+++ mangodb.js
@@ -5,22 +5,24 @@ const {
 const uri = "mongodb+srv://admin:admin@cluster0.zshub.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
-async function run() {
+async function addData() {
   try {
     await client.connect()
     const db = client.db("ark")
     const collection = db.collection('collection-1')
 
-    const data = [{
-      "test1": "1",
-      "test2": "2"
-    }]
-    await collection.insertMany(data, {
-      "ordered": true
-    })
+    // const data = [{
+    //   "test1": "1",
+    //   "test2": "2"
+    // }]
+    // await collection.insertMany(data, {
+    //   "ordered": true
+    // })
 
     const collection_updated = await collection.find({}).toArray()
+    console.log(collection_updated)
     const result = collection_updated.filter(r => r["test1"])
+
     console.log(result)
 
   } catch (e) {
@@ -30,6 +32,6 @@ async function run() {
   }
 }
 
-run().then(() => {
+addData().then(() => {
   client.close()
 })
